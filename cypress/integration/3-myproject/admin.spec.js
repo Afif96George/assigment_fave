a7a64aae93d51d720fceb7ba6d24b4ad9d3755ca
luqman <luqmanafif283@gmail.com>
2021-06-29 00:03:38 +0800

    add the lines

--- cypress/integration/3-myproject/admin.spec.js
+++ cypress/integration/3-myproject/admin.spec.js
@@ -6,22 +6,28 @@
 // https://on.cypress.io/writing-first-test
 
 describe('Login as admin', () => {
-    it('Visits the Kitchen Sink', () => {
-      cy.visit('https://www.phptravels.net/admin')
+    it('Visit the site to test', () => {
+      cy.visit('/admin')
     })
 
     it('insert email and password', () => {
     // get username
     cy.get(':nth-child(2) > input').
-    type("adadad@email.com").should('have.value', 'adadad@email.com')
+    type("admin@phptravels.com").should('have.value', 'admin@phptravels.com')
     
      // get password 
      cy.get(':nth-child(3) > input').
-     type('123abcd').should('have.value', '123abcd')
-
+     type('demoadmin').should('have.value', 'demoadmin')
+    // click button to login
      cy.get('.form-signin > .btn').click()
+     cy.wait(1000)
+    // configure the hotel first 
     })
-   
+   it('redirect to dashboard', () => {
+    cy.get('#sidebar').scrollTo(500, 0).find('#social-sidebar-menu > :nth-child(3) > a > .fa').click()
+    cy.wait(500)
+    // cy.get(' #social-sidebar-menu > :nth-child(3) > a > .fa').click()
+   })
 
 })
 
