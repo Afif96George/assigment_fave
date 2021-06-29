1008234947243c3536201955b617fd5bf9deb286
luqman <luqmanafif283@gmail.com>
2021-06-29 23:32:10 +0800

    feat: some codes

--- cypress/integration/3-myproject/admin.spec.js
+++ cypress/integration/3-myproject/admin.spec.js
@@ -5,9 +5,10 @@
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
 
+
 describe('Login as admin', () => {
     it('Visit the site to test', () => {
-      cy.visit('/admin')
+      cy.visit('admin/')
     })
 
     it('insert email and password', () => {
@@ -20,14 +21,30 @@ describe('Login as admin', () => {
      type('demoadmin').should('have.value', 'demoadmin')
     // click button to login
      cy.get('.form-signin > .btn').click()
-     cy.wait(1000)
+     cy.wait(500)
     // configure the hotel first 
     })
    it('redirect to dashboard', () => {
-    cy.get('#sidebar').scrollTo(500, 0).find('#social-sidebar-menu > :nth-child(3) > a > .fa').click()
-    cy.wait(500)
-    // cy.get(' #social-sidebar-menu > :nth-child(3) > a > .fa').click()
+    cy.get('[href="#Hotels"]').click()
+    cy.wait(1000)
+    cy.scrollTo('center')
+    cy.wait(10000)
+    cy.xpath('//*[@id="Hotels"]/li[1]/a').click()
+    cy.waitFor(10000)
    })
+
+   it('click the add button', () =>{
+     cy.wait('//*[@id="Hotels"]/li[1]/a')
+    cy.intercept('POST', 'https://www.phptravels.net/admin/hotels/add')
+    //  try {
+    //   cy.xpath('/html/body/div[3]/div/div[2]/form').click()
+    //  } catch (error) {
+    //   cy.find('.add_button > .btn').click()
+    //  }
+
+   })
+
+ 
 
 })
 
